b8097e820060526acde5d0d9b50cf8fc0cbbcdfe
JaSamLudiMoskri <78329923+JaSamLudiMoskri@users.noreply.github.com>
2021-11-06 15:45:24 +0100

    Ready for packaging

---
+++ README.md
@@ -0,0 +1,5 @@
+Every developer enjoys his Linux environment and loves to live in a terminal. Just like that, every developer hates to leave his terminal and loves quick and efficient ways to get the job done. 
+If you ever worked with AWS, you know how frustrating can be to go back and forward from the Linux environment to the AWS client and manage services. (or to use, pretty hard to learn AWS API).
+"Automaticon" is a great solver for your problems. An intuitive and quick way to manage your services without leaving the terminal. Just start the program, enter the job you want to do to a specific service, provide some prompted information, and in 10 seconds you can reboot your EC2 instance, or do whatever you want to your S3 bucket...
+At this moment "Automaticon" is only compatible with services: S3, EC2, VPC, and Security Groups.  In the future, we are leaning towards making it compatible with more service and giving it more functionality.
+Stop wasting time manually setting the AWS environment and start using "Automaticon", an automatic and time-saving friend that will make your life easier.
---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+from setuptools.command.egg_info import egg_info
+
+
+class egg_info_ex(egg_info):
+    """Includes license file into `.egg-info` folder."""
+
+    def run(self):
+        # don't duplicate license into `.egg-info` when building a distribution
+        if not self.distribution.have_run.get('install', True):
+            # `install` command is in progress, copy license
+            self.mkpath(self.egg_info)
+            self.copy_file('LICENSE.txt', self.egg_info)
+
+        egg_info.run(self)
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read()
+setup(
+    name = 'automaticon',
+    version = '0.0.1',
+    author = 'Marko Mandic',
+    author_email = 'mandicm223@gmail.com',
+    license_files = ('LICENSE',),
+    description = 'CLI tool that lets you to automaticly manage your AWS services without leaving terminal.',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = 'https://github.com/JaSamLudiMoskri/aws_automation_BOT',
+    py_modules = ['automaticon', 'app'],
+    packages = find_packages(),
+    install_requires = [requirements],
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = '''
+        [console_scripts]
+        automaticon=automaticon:main
+    '''
+)
